b2608cae14c47a3fed7c840bebc495dba157f2e4
Fernando Dingler <fdingler@amazon.com>
2022-06-18 11:11:59 -0700

    Added bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,51 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Index({ stars }) {
   return (
     <div>
-      <p>Next.js has {stars} ⭐️</p>
-      <Link href="/preact-stars">
-        <a>How about preact?</a>
-      </Link>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Welcome!
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br /><br />
+      <Container>
+        <Card>
+          <Card.Body>
+            <p>The Next.js GitHub repository currently has {stars} ⭐️ stars</p>
+            <Link href="/preact-stars">
+              <a>How about preact?</a>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const json = await res.json()
+  const res = await fetch("https://api.github.com/repos/vercel/next.js");
+  const json = await res.json();
 
   return {
+    revalidate: 10,
     props: {
       stars: json.stargazers_count,
     },
-  }
+  };
 }
 
-export default Index
+export default Index;
--- pages/preact-stars.js
+++ pages/preact-stars.js
@@ -1,25 +1,52 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function PreactStars({ stars }) {
   return (
     <div>
-      <p>Preact has {stars} ⭐</p>
-      <Link href="/">
-        <a>I bet Next.js has more stars (?)</a>
-      </Link>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Welcome!
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <br />
+      <Container>
+        <Card>
+          <Card.Body>
+            <p>Preact has {stars} ⭐ stars</p>
+            <Link href="/">
+              <a>I bet Next.js has more stars (?)</a>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://api.github.com/repos/preactjs/preact')
-  const json = await res.json()
+  const res = await fetch("https://api.github.com/repos/preactjs/preact");
+  const json = await res.json();
 
   return {
+    revalidate: 10,
     props: {
       stars: json.stargazers_count,
     },
-  }
+  };
 }
 
-export default PreactStars
+export default PreactStars;
